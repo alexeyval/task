dbe85b20e7cb569c25d8c1dfc8216addd97f86e0
Alexey Valitov <alexeyvalitov28@gmail.com>
2021-01-28 15:00:27 +0300

    Init commit

---
+++ expensive_product/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Product struct {
+	Name   string `json:"Product"`
+	Price  int
+	Rating int
+}
+
+func findExpensiveProduct(newProduct *Product, product *Product) {
+	if newProduct.Price > product.Price {
+		*product = *newProduct
+	} else if newProduct.Price == product.Price && newProduct.Rating > product.Rating {
+		*product = *newProduct
+	}
+}
+
+func readCSV(file *os.File, p *Product) {
+	parser := csv.NewReader(file)
+	if _, err := parser.Read(); err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		product, err := parser.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		Name := product[0]
+		Price, _ := strconv.Atoi(product[1])
+		Rating, _ := strconv.Atoi(product[2])
+		findExpensiveProduct(&Product{Name, Price, Rating}, p)
+	}
+}
+
+func readJSON(file *os.File, p *Product) {
+	dec := json.NewDecoder(file)
+
+	_, err := dec.Token()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for dec.More() {
+		var newProduct Product
+		err := dec.Decode(&newProduct)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		findExpensiveProduct(&newProduct, p)
+	}
+
+	_, err = dec.Token()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatal("One input file expected")
+	}
+	fileName := os.Args[1]
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	findProduct := new(Product)
+	if strings.HasSuffix(file.Name(), ".json") {
+		readJSON(file, findProduct)
+	} else if strings.HasSuffix(file.Name(), ".csv") {
+		readCSV(file, findProduct)
+	} else {
+		log.Fatal("I can handle only \".json\" and \".csv\"")
+	}
+
+	fmt.Println(*findProduct)
+}
